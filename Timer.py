df640acd07a52bc30585273b5c9b5dc5eb3ac7da
Robert Seymour <sgrseym2@liverpool.ac.uk>
2023-03-10 08:23:35 +0000

    Finally, commented.

--- Timer.py
+++ Timer.py
@@ -8,10 +8,14 @@ class Timer:
         self.coginstance = coginstance
         self.timer.start()
 
+    # This loop is called when the Timer object is initialised.
+    # Waits 5 minutes.
     @tasks.loop(minutes=5, count=1)
     async def timer(self):
         pass
 
+    # Decorator means this function is called after the loop has ended.
+    # Simply calls the end of timer function associated with main instance of the Quiz object.
     @timer.after_loop
     async def ended(self):
         await self.coginstance.end_of_timer(self.member)
